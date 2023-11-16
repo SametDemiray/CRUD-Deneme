a43228cef67d9c7713285a2a5de100e44013de3d
Samet Demiray <sametdemiray@yahoo.com>
2023-11-16 21:05:05 +0300

    PostgreSQL Data kaydetme

--- src/main/java/com/backend/crud/deneme/entities/model/Employee.java
+++ src/main/java/com/backend/crud/deneme/entities/model/Employee.java
@@ -1,5 +1,4 @@
 package com.backend.crud.deneme.entities.model;
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- src/main/java/com/backend/crud/deneme/repository/EmployeeRepository.java
+++ src/main/java/com/backend/crud/deneme/repository/EmployeeRepository.java
@@ -1,16 +1,12 @@
 package com.backend.crud.deneme.repository;
 
 import com.backend.crud.deneme.entities.model.Employee;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrimaryKeyJoinColumn;
+
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @AllArgsConstructor
 @RestController
